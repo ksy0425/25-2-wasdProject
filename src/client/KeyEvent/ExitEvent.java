7cd7c164fbcc7063fa0c0e8f9844d5c9d6f217e5
jurutang <jurutang2000@gmail.com>
2025-11-23 23:00:34 +0900

    add ExitEvent.java

---
+++ src/client/KeyEvent/ExitEvent.java
@@ -0,0 +1,30 @@
+package client.KeyEvent;
+
+import client.network.ConnectionManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ExitEvent implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            // 스트림 닫기
+            if (ConnectionManager.out != null) {
+                ConnectionManager.out.close();
+            }
+            if (ConnectionManager.in != null) {
+                ConnectionManager.in.close();
+            }
+            // 소켓 닫기
+            if (ConnectionManager.socket != null && !ConnectionManager.socket.isClosed()) {
+                ConnectionManager.socket.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("[CLIENT] 종료 중 예외 발생: " + ex.getMessage());
+        }
+        System.out.println("[CLIENT] 종료 완료. 프로그램 종료.");
+        System.exit(0);
+    }
+}
