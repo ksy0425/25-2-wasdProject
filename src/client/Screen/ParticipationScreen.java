3b36f94c61b1ab5a11e0947674a1a373bd261a0e
jurutang <jurutang2000@gmail.com>
2025-11-23 21:28:26 +0900

    modified MainScreen.java

--- src/client/Screen/ParticipationScreen.java
+++ src/client/Screen/ParticipationScreen.java
@@ -1,2 +1,11 @@
-package client.Screen;public class ParticipationScreen {
+package client.Screen;
+
+import javax.swing.*;
+
+public class ParticipationScreen extends JPanel {
+    private ClientWindow window;
+
+    public ParticipationScreen(ClientWindow window) {
+        this.window = window;
+    }
 }
